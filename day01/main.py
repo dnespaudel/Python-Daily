2c46d9d0384a85b9848eeebe3542edfcbd161bed
dnespaudel <dnespaudel10@gmail.com>
2022-03-16 12:43:34 +0545

    First Day

--- day01/main.py
+++ day01/main.py
@@ -1,16 +1,21 @@
 # Band Name Generation
 
-print('What is the name of city you were born in?')
-city_name = input()
+while True:
+    print('What is the name of city you were born in?')
+    city_name = input()
 
-if city_name == '':
-    print('You must enter the city name. Otherwise, we can not proceed.')
+    if city_name == '':
+        print('You must enter the city name. Otherwise, we can not proceed.')
+    else:
+        break
 
-print('What is your pet name?')
-pet_name = input()
+while True:
+    print('What is your pet name?')
+    pet_name = input()
 
-if pet_name == '':
-    print('You must enter your pet name. Otherwise, we can not proceed.')
+    if pet_name == '':
+        print('You must enter your pet name. Otherwise, we can not proceed.')
+    else:
+        break
 
 print('Your band name should be ' + city_name + pet_name + '.')
-
